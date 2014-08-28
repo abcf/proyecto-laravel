e6784ff19e2072e374964f2585c0f4b5c1e48a50
Mario <webmaster@abcfranquicias.es>
2014-08-28 21:55:31 +0200

    Subir proyecto

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>Test</title>
+</head>
+<body>
+	@section('sidebar')
+	CodeHero Laravel 4 Gestion de Usuarios -
+	@show
+	<div class="container">
+		@yield('content')
+	</div>
+</body>
+</html>
